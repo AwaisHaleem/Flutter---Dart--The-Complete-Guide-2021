6374b0871668ad50592a93e786f838058c7bf9d7
Awais Haleem <awaishaleem@gmail.com>
2021-11-21 16:09:36 -0800

    shop-app started with provider state management

---
+++ shop_app/lib/Widgets/product_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imgUrl;
+
+  ProductItem({required this.id, required this.title, required this.imgUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        imgUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
